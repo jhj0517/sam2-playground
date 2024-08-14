88d3da9a9743b86ad5d512731c8c537ac0e49471
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-08-14 21:12:46 +0900

    Add image save function

--- modules/utils.py
+++ modules/utils.py
@@ -1,4 +1,6 @@
 import os
+from PIL import Image
+import numpy as np
 
 
 def open_folder(folder_path: str):
@@ -6,3 +8,8 @@ def open_folder(folder_path: str):
         os.system(f'start "" "{folder_path}"')
     else:
         print(f"The folder '{folder_path}' does not exist.")
+
+
+def save_image(image_numpy: np.ndarray, output_path: str):
+    image = Image.fromarray(image_numpy)
+    image.save(output_path, "JPEG")
