cbd23dade3a99752a721bd9951734529137e16d1
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-08-14 18:12:54 +0900

    Add path

--- modules/paths.py
+++ modules/paths.py
@@ -6,10 +6,12 @@ SAM2_CONFIGS_DIR = os.path.join(WEBUI_DIR, "configs")
 OUTPUT_DIR = os.path.join(WEBUI_DIR, "outputs")
 OUTPUT_PSD_DIR = os.path.join(OUTPUT_DIR, "psd")
 TEMP_DIR = os.path.join(WEBUI_DIR, "temp")
+TEMP_OUT_DIR = os.path.join(TEMP_DIR, "out")
 
 for dir_path in [MODELS_DIR,
                  SAM2_CONFIGS_DIR,
                  OUTPUT_DIR,
                  OUTPUT_PSD_DIR,
-                 TEMP_DIR]:
+                 TEMP_DIR,
+                 TEMP_OUT_DIR]:
     os.makedirs(dir_path, exist_ok=True)
