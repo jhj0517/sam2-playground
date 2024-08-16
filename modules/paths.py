bf238b63aa320d6692e4e386d34d04aaed37de22
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-08-17 00:21:16 +0900

    Move MODEL_CONFIGS to paths

--- modules/paths.py
+++ modules/paths.py
@@ -3,6 +3,12 @@ import os
 WEBUI_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 MODELS_DIR = os.path.join(WEBUI_DIR, "models")
 SAM2_CONFIGS_DIR = os.path.join(WEBUI_DIR, "configs")
+MODEL_CONFIGS = {
+    "sam2_hiera_tiny": os.path.join(SAM2_CONFIGS_DIR, "sam2_hiera_t.yaml"),
+    "sam2_hiera_small": os.path.join(SAM2_CONFIGS_DIR, "sam2_hiera_s.yaml"),
+    "sam2_hiera_base_plus": os.path.join(SAM2_CONFIGS_DIR, "sam2_hiera_b+.yaml"),
+    "sam2_hiera_large": os.path.join(SAM2_CONFIGS_DIR, "sam2_hiera_l.yaml"),
+}
 OUTPUT_DIR = os.path.join(WEBUI_DIR, "outputs")
 OUTPUT_PSD_DIR = os.path.join(OUTPUT_DIR, "psd")
 OUTPUT_FILTER_DIR = os.path.join(OUTPUT_DIR, "filter")
@@ -17,3 +23,4 @@ for dir_path in [MODELS_DIR,
                  TEMP_DIR,
                  TEMP_OUT_DIR]:
     os.makedirs(dir_path, exist_ok=True)
+
