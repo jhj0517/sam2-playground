98ff715c54b28ecc75ed62521bd571173574a7a9
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-08-17 18:52:51 +0900

    Update html

--- modules/html_constants.py
+++ modules/html_constants.py
@@ -1,5 +1,25 @@
-DEFAULT_THEME = 'finlaymacklon/sooth_slatem'
+DEFAULT_THEME = None
 
-GITHUB_LINK = """
-## [sam2-playground](https://github.com/jhj0517/Whsiper-WebUI)
+HEADER = """
+## [sam2-playground](https://github.com/jhj0517/sam2-playground)
+
+<div>
+    <a href="">
+        <img src="https://img.shields.io/badge/GitHub-Wiki-blue?logo=github" alt="Github Wiki" style="display:inline-block;">
+    </a>
+    <a href="">
+        <img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Colab" style="display:inline-block;">
+    </a>
+</div>
+
+"""
+
+CSS = """
+#md_header a {
+  color: black;
+  text-decoration: none;
+}
+#md_header a:hover {
+  text-decoration: underline;
+}
 """
