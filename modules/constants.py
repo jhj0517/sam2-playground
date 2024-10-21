3c9529d4191a481b7ad7ad9a4a68f51d97f2edde
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-10-21 18:03:46 +0900

    Add transparent file ext constant

--- modules/constants.py
+++ modules/constants.py
@@ -7,6 +7,7 @@ COLOR_FILTER = _("Solid Color")
 SOUND_FILE_EXT = ['.mp3', '.wav', '.aac', '.flac', '.ogg', '.m4a', '.wma']
 IMAGE_FILE_EXT = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp']
 VIDEO_FILE_EXT = ['.mp4', '.avi', '.mov', '.wmv', '.flv', '.webm', '.mkv', '.mpeg', '.mpg', '.m4v', '.3gp', '.ts', '.vob']
+TRANSPARENT_VIDEO_FILE_EXT = ['.mov', '.webm']
 DEFAULT_COLOR = "#00FF00"
 DEFAULT_PIXEL_SIZE = 8
 
